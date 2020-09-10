82dd12764d181fd735ecd0909be027528558e8ec
Martin Nilsson <ibbles@hotmail.com>
2020-09-10 10:50:33 +0200

    Add 'generate' to ue.fish

--- ue.fish
+++ ue.fish
@@ -1,14 +1,14 @@
 #!/usr/bin/env fish
 
-# This script either builds or opens the Unreal Engine project whose .uproject
-# file is in the current directory. It requires CMake project files since the
-# path to the Unreal Engine binary is read from CMakeLists.txt. It always builds
-# the .+Editor build target.
+# This script either generates project files, builds or opens the Unreal Engine
+# project whose .uproject file is in the current directory. It requires CMake
+# project files since the path to the Unreal Engine binary is read from
+# CMakeLists.txt. It always builds the .+Editor build target.
 #
-# Takes one argument, which should be either 'build' or 'open'.
+# Takes one argument, which should be either 'generate', 'build' or 'open'.
 
 function print_usage
-    echo "Usage: $argv[0] build|open"
+    echo "Usage: $argv[0] generate|build|open"
     exit 1
 end
 
@@ -18,6 +18,10 @@ if  test $num_args -ne 1
     print_usage
 end
 
+
+function generate_project
+    eval "$ue_generate" "$project_path" -CMakefile -QMakefile -Makefile -game
+end
 
 function build_project
     make "$target_name"
@@ -31,10 +35,16 @@ end
 
 set ue_root (grep add_custom_target CMakeLists.txt | head -n1 | cut -d '"' -f2)
 set ue_binary $ue_root/Engine/Binaries/Linux/UE4Editor
+set ue_generate $ue_root/GenerateProjectFiles.sh
 if not type -q "$ue_binary"
     echo "Unreal Engine Editor binary $ue_binary is not executable."
     exit 1
 end
+if not type -q "$ue_generate"
+   echo "Unreal Engine project generator script $ue_generate is not executable."
+   exit 1
+end
+
 
 # TODO: Only aloow a single .uproject file.
 
@@ -43,6 +53,8 @@ set project_name (basename "$project_path" .uproject)
 set target_name $project_name"Editor"
 
 switch $argv[1]
+    case generate
+         generate_project
     case build
          build_project
     case open
