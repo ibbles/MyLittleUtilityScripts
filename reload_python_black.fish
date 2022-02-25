c849620fcef4a9970858ab49b6dc251cde1ded1b
Martin Nilsson <ibbles@hotmail.com>
2022-02-25 17:14:03 +0100

    Use find instead of *.py **/*.py for listening for files to format with black

--- reload_python_black.fish
+++ reload_python_black.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 while true
-    set file (inotifywait -qe close_write *.py **/*.py | cut -d ' ' -f 1)
+    set file (inotifywait -qe close_write (find . -iname "*.py") | cut -d ' ' -f 1)
     echo -e "\n\nFormatting $file."
     black "$file"
     flake8 "$file" --ignore=E501
