b0a03795233c67648a8f32f12bc3e91134c1c7a2
Martin Nilsson <ibbles@hotmail.com>
2022-03-02 17:34:43 +0100

    Pass --preview to black to enable string formatting
    Add more --ignore to flake8 and describe why

--- reload_python_black.fish
+++ reload_python_black.fish
@@ -1,9 +1,20 @@
 #!/usr/bin/env fish
 
+if not type -q black
+    echo "Black is not installed. Install with" >&2
+    echo "  pip3 install black"
+    exit 1
+end
+
 while true
-    set file (inotifywait -qe close_write *.py **/*.py | cut -d ' ' -f 1)
-    echo -e "\n\nFormatting $file."
-    black "$file"
-    flake8 "$file" --ignore=E501
+    set file (inotifywait -qe close_write *.py | cut -d ' ' -f 1)
+    echo -e "\n\n\n\nFormatting $file."
+    black "$file" --preview
+
+    # E203: Whitespace before ':'.
+    # https://github.com/psf/black/issues/315
+    # W503: Line break efter binary operator.
+    # https://www.python.org/dev/peps/pep-0008/#should-a-line-break-before-or-after-a-binary-operator
+    flake8 "$file" --ignore=E203,W503 --max-line-length=88
     sleep 1
 end
