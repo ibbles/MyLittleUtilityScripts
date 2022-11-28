4f5309d54234cf3178c28b0f20225de0cf782047
Martin Nilsson <ibbles@hotmail.com>
2022-11-28 19:37:47 +0100

    Add mn_images_to_mp4.fish, a script that uses ffmpeg to convert images to a video

---
+++ mn_images_to_mp4.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/fish
+
+set pattern $argv[1]
+echo "argv[1]: '$argv[1]'."
+
+if test -z "$pattern"
+    echo "Usage: $argv[1] PATTERN"
+    echo "Where pattern is e.g '*.jpg'."
+    echo "Take care to have the pattern being passed and not expanded by the shell."
+    exit 1
+end
+
+if test -f output.mp4
+    echo "Output file already exitst."
+    exit 1
+end
+
+ffmpeg -framerate 60 -pattern_type glob -i $pattern -c:v libx264 -r 60 -pix_fmt yuv420p output.mp4
