dc77ad34dead22a90c8343cad90001da6769eeb8
Martin Nilsson <ibbles@hotmail.com>
2022-12-12 15:49:31 +0100

    Add limit parameter to mn_git_log.fish

--- mn_git_log.fish
+++ mn_git_log.fish
@@ -1,3 +1,9 @@
 #!/usr/bin/env fish
 
-git log --pretty=format:"%C(auto) %h %an %ar %Cgreen %s" -10
+set num 10
+if test -n "$argv[1]"
+    set num $argv[1]
+end
+
+echo 'git log --pretty=format:"%C(auto) %h %an %ar %Cgreen %s" -'$num
+git log --pretty=format:"%C(auto) %h %an %ar %Cgreen %s" -$num
