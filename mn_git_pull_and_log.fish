53dfca6750b6dcc98d35697f6a5e2797aa1195eb
Martin Nilsson <ibbles@hotmail.com>
2022-12-12 15:45:01 +0100

    Use mn_git_removed_branches.fish instead of gitListRemovedBranches in mn_git_pull_and_log.fish

--- mn_git_pull_and_log.fish
+++ mn_git_pull_and_log.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-git pull --prune ;and echo -e "\n" ;and gitListRemovedBranches ;and echo -e "\n" ;and git log --pretty=format:"%C(auto) %h %an %ar %Cgreen %s" -10
+git pull --prune ;and echo -e "\n" ;and mn_git_removed_branches.fish ;and echo -e "\n" ;and git log --pretty=format:"%C(auto) %h %an %ar %Cgreen %s" -10
