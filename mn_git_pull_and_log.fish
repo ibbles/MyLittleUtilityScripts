cebea5f2dd711f6c5fae1f3e1323923b2d572cb0
Martin Nilsson <ibbles@hotmail.com>
2023-02-02 08:33:54 +0100

    Add command prints to mn_git_pull_and_log.fish and mn_git_removed_branches.fish

--- mn_git_pull_and_log.fish
+++ mn_git_pull_and_log.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 
+echo "git pull --prune"
 git pull --prune
 echo -e "\n"
 mn_git_removed_branches.fish
--- mn_git_removed_branches.fish
+++ mn_git_removed_branches.fish
@@ -1,3 +1,4 @@
 #!/usr/bin/env fish
 
+echo "git branch -vv | grep \"\[.*: gone\]\" | awk '{print $1, $3, $4}'"
 git branch -vv | grep "\[.*: gone\]" | awk '{print $1, $3, $4}'
