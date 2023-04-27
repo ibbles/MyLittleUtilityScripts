ad23adb0703a5806be614b57b35ebad89177aee6
Martin Nilsson <ibbles@hotmail.com>
2023-04-27 18:45:53 +0200

    In mn_tangent.fish, include in error message the possibility that there may be no installation

--- mn_tangent.fish
+++ mn_tangent.fish
@@ -2,7 +2,8 @@
 
 set num_matches (count ~/bin/Tangent-*.AppImage)
 if test "$num_matches" -ne "1"
-    echo "Found multiple Tangent installations, don't know which one to launch." 1>&2
+    echo "Found multiple (or no) Tangent installations, don't know which one to launch." 1>&2
+    ls -1 ~/bin/Tangent-*.AppImage
     exit 1
 end
 ~/bin/Tangent-*.AppImage $argv
