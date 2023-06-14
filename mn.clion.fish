c4b0e877184dc3a5365e2a61a0a170daf64c6ee7
Martin Nilsson <ibbles@hotmail.com>
2023-06-14 20:18:17 +0200

    Use a CLion directory for CLion installations

--- mn.clion.fish
+++ mn.clion.fish
@@ -1,9 +1,9 @@
 #!/usr/bin/env fish
 
-set num_matches (count ~/bin/clion-*)
+set num_matches (count ~/bin/CLion/clion-*)
 if test "$num_matches" -ne "1"
     echo "Found multiple (or no) CLion installations, don't know which one to launch." 1>&2
-    ll -d ~/bin/clion-*
+    ll -d ~/bin/CLion/clion-*
     exit 1
 end
-~/bin/clion-*/bin/clion.sh $argv
+~/bin/CLion/clion-*/bin/clion.sh $argv
