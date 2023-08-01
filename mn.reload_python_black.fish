1abcb143517a7c576964ed8cf1893fa61acf3676
Martin Nilsson <ibbles@hotmail.com>
2023-08-01 16:32:06 +0200

    Add --linelength and --noformat parameters to mn.reload_python_black.fish

--- mn.reload_python_black.fish
+++ mn.reload_python_black.fish
@@ -3,16 +3,30 @@
 
 if not type -q black
     echo "Black is not installed. Install with" >&2
-    echo "  <Figure out how to detect the Python binary currently executing, then print:>"
-    echo "       $PATH_TO_PYTHON -m pip install -U black"
-    echo "  pip3 install black"
+    echo "  <Figure out how to detect the Python binary currently executing, then print:>" >&2
+    echo "       $PATH_TO_PYTHON -m pip install -U black" >&2
+    echo "  pip3 install black" >&2
     exit 1
 end
 
 if not type -q flake8
     echo "Flake8 is not installed. Install with" >&2
-    echo "  sudo apt install flake8"
+    echo "  sudo apt install flake8" >&2
     exit 1
+end
+
+argparse --name=mn.reload_python_black 'h/help' 'noformat' 'linelength=' -- $argv
+or exit 1
+
+if test -n "$_flag_help"
+   echo "Usage:"
+   echo "mn.reload_python_black.fish --linelength=100"
+   exit 0
+end
+
+set linelength 88
+if test -n "$_flag_linelength"
+   echo "Line length: $_flag_linelength"
 end
 
 while true
@@ -21,13 +35,16 @@ while true
     if test -z "$file"
         continue
     end
-    echo -e "\n\n\n\nFormatting $file."
-    black "$file" --preview
+
+    if test -z "$_flag_noformat"
+        echo -e "\n\n\n\nFormatting $file."
+        black "$file" --preview
+    end
 
     # E203: Whitespace before ':'.
     # https://github.com/psf/black/issues/315
     # W503: Line break efter binary operator.
     # https://www.python.org/dev/peps/pep-0008/#should-a-line-break-before-or-after-a-binary-operator
-    flake8 "$file" --ignore=E203,W503 --max-line-length=88
+    flake8 "$file" --ignore=E203,W503 --max-line-length=$_flag_linelength
     sleep 1
 end
