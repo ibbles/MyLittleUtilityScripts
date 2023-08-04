8121e88132ebc4d65f53daa310892d0d9b465e01
Martin Nilsson <ibbles@hotmail.com>
2023-08-04 13:12:26 +0200

    In mn.clion.fish, default to open the current directory

--- mn.clion.fish
+++ mn.clion.fish
@@ -6,4 +6,13 @@ if test "$num_matches" -ne "1"
     ll -d ~/bin/clion/clion-*
     exit 1
 end
-~/bin/clion/clion-*/bin/clion.sh $argv
+
+set clion_binary ~/bin/clion/clion-*/bin/clion.sh
+
+if test (count $argv) -eq 0
+    echo "$clion_binary" (readlink -f .)
+    "$clion_binary" (readlink -f .) >/dev/null 2>&1 &
+else
+    echo "$clion_binary" $argv
+    "$clion_binary" $argv >/dev/null 2>&1 &
+end
