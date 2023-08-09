8e471cf699a0c10b8901bc38b0b2721b28f345dd
Martin Nilsson <ibbles@hotmail.com>
2023-08-09 21:36:21 +0200

    In mn.git_log_unpushed.fish, don't assume branch name

--- mn.git_log_unpushed.fish
+++ mn.git_log_unpushed.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-echo "git log --pretty=format:\"%C(auto) %h %an %ar %Cgreen %s\" origin/master..HEAD"
-git log --pretty=format:"%C(auto) %h %an %ar %Cgreen %s" origin/master..HEAD
+echo "git log --pretty=format:\"%C(auto) %h %an %ar %Cgreen %s\" origin/"(mn.git_branch_id.fish)"..HEAD"
+git log --pretty=format:"%C(auto) %h %an %ar %Cgreen %s" origin/(mn.git_branch_id.fish)..HEAD
