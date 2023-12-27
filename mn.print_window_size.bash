558c515e3b2037e4dcd75382a13baae944457688
Martin Nilsson <ibbles@hotmail.com>
2023-12-27 15:43:55 +0100

    Simplify mn.print_window_size.bash

--- mn.print_window_size.bash
+++ mn.print_window_size.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Select the window to print the size of"
+# This, the xev bit, deadlocks on Ubuntu 22.04. Why?
+# echo "Select the window to print the size of"
+# window=`xdotool selectwindow`
+# echo "Selected window $(xdotool getwindowname ${window})"
+# xev -id ${window} -event structure | grep width
 
-window=`xdotool selectwindow`
-echo "Selected window $(xdotool getwindowname ${window})"
-xev -id ${window} -event structure | grep width
+# This doesn't format the output quite the same,
+# but it works.
+xwininfo | grep -e Width -e Height
