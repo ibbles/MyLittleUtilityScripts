ec93cb2860c0ce09c877922d7d26b1e6deb32843
Martin Nilsson <ibbles@hotmail.com>
2024-05-28 08:04:53 +0200

    In mn.VRAM_usage.fish, write a header and place the file in $HOME rather than /tmp/ to ensure it survives reboots

--- mn.VRAM_usage.fish
+++ mn.VRAM_usage.fish
@@ -1,11 +1,19 @@
 #!/usr/bin/env fish
 
+set file "$HOME/VRAM_usage.dat"
+
+if test -f "$file"
+   # echo "File already exists, not writing header."
+else
+   # echo "File created, writing header."
+   echo "Time Total XOrg" > "$file"
+end
 
 while true
     set used (nvidia-smi --query --display=MEMORY | grep "FB Memory Usage" -A3 | grep "Used" | tr -s ' ' | cut -d ' ' -f 4)
     set xorg (nvidia-smi | grep Xorg | awk '{print $8}' | cut -d 'M' -f1)
     #set timestamp (date "+%Y-%m-%dT%H:%M:%S")
     set timestamp (date "+%s")
-    echo $timestamp $used $xorg >> /tmp/VRAM_usage.dat
-    sleep 60
+    echo $timestamp $used $xorg >> "$file"
+    sleep 600
 end
