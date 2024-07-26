1068dd72ef0550805cfebae1fe848dab6f2f9eed
Martin Nilsson <ibbles@hotmail.com>
2024-07-26 18:02:44 +0200

    In mn.timer.bash, fix count-down display when setting a more-than-a-minute timer using seconds

--- mn.timer.bash
+++ mn.timer.bash
@@ -12,9 +12,11 @@ now=`date +%s`
 last_char="${duration: -1}"
 if [ "${last_char}" == "s" ] ; then
     duration_seconds="${duration%s}"
+    duration_minutes="$((duration_seconds / 60))"
     later=$((${now} + ${duration_seconds} ))
 elif [ "${last_char}" == "m" ] ; then
     duration_minutes="${duration%m}"
+    duration_seconds="$((duration_minutes * 60))"
     later=$(($now + ($duration_minutes * 60)))
 else
     # Not 's' or 'm' suffix. For now just assume it to be a number in minutes.
@@ -24,13 +26,10 @@ fi
 # Between each print we clear the line by first backspacing
 # to the beginning of the line, then overwriting anything
 # that may remain after the cursor with blanks, and then
-# backspacing again.  The number below should be larger than
+# backspacing again. The number below should be larger than
 # the length of the longest printed line.
 backspace=$(head -c 64 < /dev/zero | tr '\0' '\b')
 blank=$(head -c 64 < /dev/zero | tr '\0' ' ')
-
-# For daft platforms, use 'dd if=/dev/zero bs=1 count=64' for
-# the first command.
 
 while [ `date +%s` -lt $later ] ; do
     # Clear the line for a new print.
@@ -41,7 +40,7 @@ while [ `date +%s` -lt $later ] ; do
     remaining=$(($later - `date +%s`))
     if [ $remaining -gt 61 ] ; then
         to_sleep=$(( ($remaining % 60) + 1 ))
-        echo -n "$(($remaining / 60))/$duration min: $title."
+        echo -n "$(($remaining / 60))/$duration_minutes min: $title."
         sleep $to_sleep
     else
         to_sleep=1
