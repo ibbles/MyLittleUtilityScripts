671d74f1cb8a5dd7eb460a54eea0ccd6b5c6dcf4
Martin Nilsson <ibbles@hotmail.com>
2024-10-07 08:24:45 +0200

    In mn.git_pull_and_log, ask for confirmation before making changes

--- mn.git_pull_and_log.fish
+++ mn.git_pull_and_log.fish
@@ -12,6 +12,11 @@ echo -e '\ngit log  --pretty=format:"%C(auto) %h %an %aI %Cgreen %s" HEAD..origi
 git log  --pretty=format:"%C(auto) %h %an %aI %Cgreen %s" HEAD..origin/(mn.git_branch_id.fish)
 echo -e '\ngit log --pretty=format:"%C(auto) %h %an %ar %Cgreen %s" origin/'(mn.git_branch_id.fish)'..HEAD'
 git log --pretty=format:"%C(auto) %h %an %ar %Cgreen %s" origin/(mn.git_branch_id.fish)..HEAD
+read -P "Continue? [y/n] " line
+if test "$line" != "y"
+    echo "Doing nothing."
+    exit 9
+end
 echo -e "\ngit pull --prune"
 git pull --prune
 echo -e "\n"
