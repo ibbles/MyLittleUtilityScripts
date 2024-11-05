e768949b27ed1dd1c4ef8d8e13a7a2d7eea99318
Martin Nilsson <ibbles@hotmail.com>
2024-11-05 20:49:26 +0100

    In mn.tangent.fish, don't allow opening the home directory

--- mn.tangent.fish
+++ mn.tangent.fish
@@ -1,9 +1,19 @@
 #!/usr/bin/env fish
 
+# Set up logging.
+set logfile /tmp/mn.tangent.fish.log
+echo "" > /tmp/mn.tangent.fish.log
+function log
+    echo -e $argv
+    echo -e (date) " " $argv >> $logfile
+end
+
+log "mn.tangent.fish starting"
+
 # Make sure we have exactly one active Tangent Notes installation.
 set num_matches (count ~/bin/Tangent-*.AppImage)
 if test "$num_matches" -ne "1"
-    echo "Found multiple (or no) Tangent installations, don't know which one to launch." 1>&2
+    log "Found multiple (or no) Tangent installations, don't know which one to launch." 1>&2
     ls -1 ~/bin/Tangent-*.AppImage
     exit 1
 end
@@ -12,10 +22,22 @@ end
 # with an absolute path.
 if test (count $argv) -gt 0
     # Got arguments, pass then on unchanged.
-    echo ~/bin/Tangent-*.AppImage $argv
+    log ~/bin/Tangent-*.AppImage $argv
     ~/bin/Tangent-*.AppImage $argv >/dev/null 2>&1 &
 else
-    # No arguments, open the current directory.
-    echo ~/bin/Tangent-*.AppImage (readlink -f .)
-    ~/bin/Tangent-*.AppImage (readlink -f .) >/dev/null 2>&1 &
+    # No arguments, open the current directory unless it is the home directory.
+    set directory (readlink -f .)
+    log "comparing\n$directory\nand\n$HOME\n"
+    if test "$directory" = "$HOME"
+        # Don't open the home directory. That is often accidental and creates
+        # a Tangent workspace that is very large and causes every folder we try
+        # to open within the home directory to instead open the home directory.
+        # May lead to initialization failure and an unusable Tangent, until
+        # the ~/.tangent folder is cleared.
+        log ~/bin/Tangent-*.AppImage
+        ~/bin/Tangent-*.AppImage 2>&1 &
+    else
+        log ~/bin/Tangent-*.AppImage "$directory"
+        ~/bin/Tangent-*.AppImage (readlink -f .) >/dev/null 2>&1 &
+    end
 end
