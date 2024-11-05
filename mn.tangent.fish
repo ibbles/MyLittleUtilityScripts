cf419c06bbec2c73d091af7dc3a22769cb9e56bb
Martin Nilsson <ibbles@hotmail.com>
2024-11-05 20:54:25 +0100

    In mn.tangent.fish, prepare for supporting multiple Tangent installations

--- mn.tangent.fish
+++ mn.tangent.fish
@@ -11,6 +11,7 @@ end
 log "mn.tangent.fish starting"
 
 # Make sure we have exactly one active Tangent Notes installation.
+# TODO Allow multiple installations, use the most recent.
 set num_matches (count ~/bin/Tangent-*.AppImage)
 if test "$num_matches" -ne "1"
     log "Found multiple (or no) Tangent installations, don't know which one to launch." 1>&2
@@ -18,26 +19,27 @@ if test "$num_matches" -ne "1"
     exit 1
 end
 
+set tangent ~/bin/Tangent-*.AppImage
+
 # Either open the given directory / file, or the current directory
 # with an absolute path.
 if test (count $argv) -gt 0
-    # Got arguments, pass then on unchanged.
+    # Got arguments, pass them on unchanged.
     log ~/bin/Tangent-*.AppImage $argv
-    ~/bin/Tangent-*.AppImage $argv >/dev/null 2>&1 &
+    command $tangent $argv >/dev/null 2>&1 &
 else
     # No arguments, open the current directory unless it is the home directory.
     set directory (readlink -f .)
-    log "comparing\n$directory\nand\n$HOME\n"
     if test "$directory" = "$HOME"
         # Don't open the home directory. That is often accidental and creates
         # a Tangent workspace that is very large and causes every folder we try
         # to open within the home directory to instead open the home directory.
         # May lead to initialization failure and an unusable Tangent, until
         # the ~/.tangent folder is cleared.
-        log ~/bin/Tangent-*.AppImage
-        ~/bin/Tangent-*.AppImage 2>&1 &
+        log $tangent
+        command $tangent 2>&1 &
     else
-        log ~/bin/Tangent-*.AppImage "$directory"
-        ~/bin/Tangent-*.AppImage (readlink -f .) >/dev/null 2>&1 &
+        log $tangent "$directory"
+        command $tangent "$directory" >/dev/null 2>&1 &
     end
 end
