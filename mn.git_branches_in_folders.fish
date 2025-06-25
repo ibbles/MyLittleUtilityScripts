12c6b2354b11a77a9840b9d4febb988fb12b1204
Martin Nilsson <ibbles@hotmail.com>
2025-06-25 12:53:31 +0200

    Make mn.git_branches_in_folders.fish search recursively

--- mn.git_branches_in_folders.fish
+++ mn.git_branches_in_folders.fish
@@ -1,5 +1,14 @@
 #!/usr/bin/fish
 
-for dir in (find . -mindepth 1 -maxdepth 1 -type d)
-    echo -e "$dir"\t(git -C "$dir" rev-parse --abbrev-ref HEAD 2>/dev/null)
-end | sort
+# Variant that only looks in immediate directories.
+#for dir in (find . -mindepth 1 -maxdepth 1 -type d)
+#    echo -e "$dir"\t(git -C "$dir" rev-parse --abbrev-ref HEAD 2>/dev/null)
+#end | sort
+
+
+# Variant that finds all Git repositories.
+for git_dir in (find . -iname "*.git" -type d)
+    set dir (dirname "$git_dir")
+    echo -en "$dir:\n  "
+    git -C "$dir" rev-parse --abbrev-ref HEAD 2>/dev/null
+end
