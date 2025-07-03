d927c279206bf30bc71c41cb32c03049ac8cf8f0
Martin Nilsson <ibbles@hotmail.com>
2025-07-03 11:17:18 +0200

    Add 'snap refresh' to mn.system_update.fish

--- mn.system_update.fish
+++ mn.system_update.fish
@@ -4,3 +4,4 @@ echo -e "\n\nUpdate:" ;and sudo apt update \
      ;and echo -e "\n\nUpgrade:" ;and sudo apt upgrade \
      ;and echo -e "\n\nFull upgrade:" ;and sudo apt full-upgrade \
      ;and echo -e "\n\nStill remaining:" ;and apt list --upgradable
+     ;and echo -e "\n\nSnaps:" ;and sudo snap refresh
