a0ec65cc5bef4f398bd314a59e7e2c1f39bce187
Martin Nilsson <ibbles@hotmail.com>
2025-07-03 11:13:30 +0200

    Add mn.system_update.fish

---
+++ mn.system_update.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/fish
+
+echo -e "\n\nUpdate:" ;and sudo apt update \
+     ;and echo -e "\n\nUpgrade:" ;and sudo apt upgrade \
+     ;and echo -e "\n\nFull upgrade:" ;and sudo apt full-upgrade \
+     ;and echo -e "\n\nStill remaining:" ;and apt list --upgradable
