bfb2f5566d29e2ee3b88655390dc2a073af94c34
Martin Nilsson <ibbles@hotmail.com>
2025-07-08 19:49:35 +0200

    Add trace output to mn.sleep_in_loop.fish
    It seems sometimes it doesn't sleep properly.

--- mn.sleep_in_loop.bash
+++ mn.sleep_in_loop.bash
@@ -14,6 +14,7 @@ echo -e "\n\n\n\n\n         Sleeping in loop.\n\n              CTRL+C to cancel.
 
 
 function turn_off {
+    echo "Turn off."
     xset dpms force off
 }
 
@@ -27,6 +28,7 @@ turn_off
 while true ; do
     sleep 10
     if test_on ; then
+        echo "We are on, turning off in 5 s."
         sleep 5
         turn_off
     fi
