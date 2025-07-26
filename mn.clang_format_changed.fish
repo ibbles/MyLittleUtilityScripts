664bad33386e8c620d5f8adb4eb5a96c9d9b7881
Martin Nilsson <ibbles@hotmail.com>
2025-07-26 20:09:11 +0200

    In mn_clang_format_changed.fish, fix changed files pattern

--- mn.clang_format_changed.fish
+++ mn.clang_format_changed.fish
@@ -2,15 +2,25 @@
 
 # Find the header and source files that have local modifications.
 set files (git status --short --porcelain | \
-    grep  -e "\.cpp\$" -e "\.h\$" | \
-    # grep -e "^M  " -e "^MM "  -e "^A  " -e "^AM " -e "^R  " | \
+    grep -e "\.cpp\$" -e "\.h\$" | \
+    grep -e "^ *M *" -e "^ MM "  -e "^ *A *" -e "^AM " -e "^ *R *" | \
     awk '{print $2}')
 
 echo "Clang Format"
-printArgs.bash $files
+mn.print_args.bash $files
+echo "?"
+
+set repo_root (git rev-parse --show-toplevel)
+set full_files
+for file in $files
+    set full_files $full_files "$repo_root"/"$file"
+end
+
+echo "Clang Format"
+mn.print_args.bash $full_files
 echo "?"
 
 read -P "[y/n]" do_it
 if test "$do_it" = "y"
-   clang-format -i -style=file $files
+   clang-format -i -style=file $full_files
 end
