a22b456686339ea5c236f26b5876c92c051a77a8
Martin Nilsson <ibbles@hotmail.com>
2025-08-19 20:34:02 +0200

    Add directory argument to mn.clang_format_recursive.bash

--- mn.clang_format_recursive.bash
+++ mn.clang_format_recursive.bash
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-for file in $(find . \( -iname "*.h" -or -iname "*.hpp" -or -iname "*.cpp" \) ) ; do
+if [ $# -gt 1 ] ; then
+    echo "Too many arguments." 1>&2
+    exit 1
+elif [ $# -eq 1 ] ; then
+    dir="$1"
+else
+    dir="."
+fi
+
+if [ ! -d "$dir" ] ; then
+    echo "'$dir' is not a directory." 1>&2
+    exit 1
+fi
+
+read -p "Format all C++ source files in $dir? [y/n] " input
+if [ "$input" != "y" ] ; then
+    exit 0
+fi
+
+for file in $(find "$dir" \( -iname "*.h" -or -iname "*.hpp" -or -iname "*.cpp" \) ) ; do
     clang-format -style=file -i "$file"
  done
