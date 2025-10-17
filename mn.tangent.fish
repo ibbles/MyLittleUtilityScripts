0289bdd632b3eb7441d348be1603abeef873038b
Martin Nilsson <ibbles@hotmail.com>
2025-10-17 22:23:10 +0200

    In mn.tangent.fish, support multiple Tangent Notes executables

--- mn.tangent.fish
+++ mn.tangent.fish
@@ -10,16 +10,31 @@ end
 
 log "mn.tangent.fish starting"
 
-# Make sure we have exactly one active Tangent Notes installation.
-# TODO Allow multiple installations, use the most recent.
-set num_matches (count ~/bin/Tangent-*.AppImage)
-if test "$num_matches" -ne "1"
-    log "Found multiple (or no) Tangent installations, don't know which one to launch." 1>&2
-    ls -1 ~/bin/Tangent-*.AppImage
-    exit 1
-end
 
-set tangent ~/bin/Tangent-*.AppImage
+# Determine if we should use a versioned or non-versioned file name
+# for the Tangent binary.
+#
+# For a while I only used versioned file names, but that got complicated
+# with AppArmor since I had to update /etc/apparmor.d/tangent_local and
+# run
+#   sudo systemctl reload apparmor.service
+# all the time. By having a fixed file name regardless of the application
+# version the AppArmor file can stay the same even across application
+# upates.
+if test -f ~/bin/Tangent.AppImage
+    set tangent ~/bin/Tangent.AppImage
+else
+    # Make sure we have exactly one active Tangent Notes installation.
+    # TODO Allow multiple installations, use the most recent.
+    set num_matches (count ~/bin/Tangent-*.AppImage)
+    if test "$num_matches" -ne "1"
+        log "Found multiple (or no) Tangent installations, don't know which one to launch." 1>&2
+        ls -1 ~/bin/Tangent-*.AppImage
+        exit 1
+    end
+
+    set tangent ~/bin/Tangent-*.AppImage
+end
 
 # Either open the given directory / file, or the current directory
 # with an absolute path.
