ed5239e00e716e78be8bb737628d08d055e231f2
Martin Nilsson <ibbles@hotmail.com>
2025-10-18 12:01:53 +0200

    Add comments to mn.youtube.fish

--- mn.youtube.fish
+++ mn.youtube.fish
@@ -1,10 +1,12 @@
 #!/usr/bin/env fish
 
+# Read command line parameter, which is number of minutes to run.
 set time $argv[1]
 if test -z "$time"
     set time 30
 end
 
+# Chose a browser and use it to open YouTube.
 set browser_exe "vivaldi-stable"
 $browser_exe www.youtube.com 2>/dev/null &
 
@@ -14,6 +16,8 @@ $browser_exe www.youtube.com 2>/dev/null &
 # if the browser is still running. If not, then exit immediately.
 mn.timer.bash "$time"m
 
+# Timer epired, let the user know we're about to close the browser.
 dialog --msgbox "Time's Up, YouTube closing" 10 50
 
+# Send polite quit request to the browser.
 kill %1
