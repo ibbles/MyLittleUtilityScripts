3df23294c465c546fcf86afdebbd216f43bffc28
Martin Nilsson <ibbles@hotmail.com>
2025-10-18 11:59:49 +0200

    In mn.youtube.fish, use mn.timer.bash instead of sleep
    So we get a count-down in the terminal.

--- mn.youtube.fish
+++ mn.youtube.fish
@@ -9,7 +9,11 @@ set browser_exe "vivaldi-stable"
 set browser_bin "vivaldi-bin"
 $browser_exe www.youtube.com 2>/dev/null & disown
 
-sleep "$time"m
+# Wait for the timer.
+#
+# TODO: Instead of blanket waiting, wake up periodically and check
+# if the browser is still running. If not, then exit immediately.
+mn.timer.bash "$time"m
 
 dialog --msgbox "Time's Up, YouTube closing" 10 50
 
