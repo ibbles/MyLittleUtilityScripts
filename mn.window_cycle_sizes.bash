8e4ffc0a2a483bfa99baa71c73d61c38574cbca2
Martin Nilsson <ibbles@hotmail.com>
2025-11-10 11:10:28 +0100

    Make mn.window_cycle_sizes.bash actually cycle sizes

--- mn.window_cycle_sizes.bash
+++ mn.window_cycle_sizes.bash
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+#set -x
+
+direction=smaller
+if [ "$1" == "--larger" ] ; then
+    direction=larger
+fi
+
 window=`xdotool getactivewindow`
 window_position=`xdotool getwindowgeometry ${window} | grep "Position:" | tr -s ' ' | cut -d ' ' -f3 | cut -d ',' -f1`
 window_width=`xdotool getwindowgeometry ${window} | grep "Geometry:" | tr -s ' ' | cut -d ' ' -f3 | cut -d 'x' -f1`
+window_height=`xdotool getwindowgeometry ${window} | grep "Geometry:" | tr -s ' ' | cut -d ' ' -f3 | cut -d 'x' -f2`
 window_center=$(($window_position + ($window_width / 2)))
 
 window_left=$window_position
-window_width=1920
-window_height=1080
+
+if [ "$direction" == "smaller" ] ; then
+    window_width=$(($window_width * 100 / 120))
+else
+    window_width=$(($window_width * 100 / 80))
+fi
 
 # Can only resize windows that aren't maximized.
 wmctrl -r :ACTIVE: -b remove,maximized_vert
