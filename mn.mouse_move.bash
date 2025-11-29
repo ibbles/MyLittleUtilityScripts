0529e5c00e87c64e3f630ec6998612136c0f720e
Martin Nilsson <ibbles@hotmail.com>
2025-11-29 16:44:28 +0100

    Add mn.mouse_move.bash

---
+++ mn.mouse_move.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#xdotool mousemove 2562 0
+#xdotool mousemove 2500 1600
+#xdotool mousemove 2000 1599
+#xdotool mousemove 1000 3840
+#xdotool mousemove 1853 3840
+#xdotool mousemove 2000 2000
+xdotool mousemove 2159 3839
+
