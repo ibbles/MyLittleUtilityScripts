40f8bf7477ca59d72288a68f21b64c2db3f98ed1
Martin Nilsson <ibbles@hotmail.com>
2026-01-23 08:37:08 +0100

    Add mn.git_lfs_track_files.fish

---
+++ mn.git_lfs_track_files.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+
+
+git lfs track "*.scdoc" "*.zip" "*.urdf" "*.dae" "*.agx" "*.obj" "*.fbx" "*.blend*" "*.blend" "*.jpg" "*.png" "*.umap" "*.uasset" "*.mp4" "*.avi" "*.xcf" "*.psd" "*.tiff" "*.tif" "*.tga" "*.exr"
