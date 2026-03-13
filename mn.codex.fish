91879320699f6cb91605782a29d0850fcb8d1585
Martin Nilsson <ibbles@hotmail.com>
2026-03-13 10:50:50 +0100

    In mn.codex.fish, run codex directly in the container

--- mn.codex.fish
+++ mn.codex.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-docker run -i -t --rm=true --user (id -u):(id -g) -v ~/.codex:/codex-home/ -v (realpath .):/cwd -v /media/s2000/UnrealEngine/5.7.2:/UnrealEngine:ro -e CODEX_HOME=/codex-home $argv codex #/bin/bash -c "exec codex"
+docker run -i -t --rm=true --user (id -u):(id -g) -v ~/.codex:/codex-home/ -v (realpath .):/cwd -v /media/s2000/UnrealEngine/5.7.2:/UnrealEngine:ro -e CODEX_HOME=/codex-home $argv codex /bin/bash -c "exec codex"
 
