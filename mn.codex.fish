5ed620cc233a3c33135e5b4365bdd76d37dee909
Martin Nilsson <ibbles@hotmail.com>
2026-03-16 08:31:10 +0100

    In mn.codex.fish, disable running codex in the container
    Sometimes I want to access files and such instead.

--- mn.codex.fish
+++ mn.codex.fish
@@ -1,4 +1,5 @@
 #!/usr/bin/env fish
 
-docker run -i -t --rm=true --user (id -u):(id -g) -v ~/.codex:/codex-home/ -v (realpath .):/cwd -v /media/s2000/UnrealEngine/5.7.2:/UnrealEngine:ro -e CODEX_HOME=/codex-home $argv codex /bin/bash -c "exec codex"
+echo docker run -i -t --rm=true --user (id -u):(id -g) -v ~/.codex:/codex-home/ -v (realpath .):/cwd -v /media/s2000/UnrealEngine/5.7.2:/UnrealEngine:ro -e CODEX_HOME=/codex-home $argv codex
+docker run -i -t --rm=true --user (id -u):(id -g) -v ~/.codex:/codex-home/ -v (realpath .):/cwd -v /media/s2000/UnrealEngine/5.7.2:/UnrealEngine:ro -e CODEX_HOME=/codex-home $argv codex
 
