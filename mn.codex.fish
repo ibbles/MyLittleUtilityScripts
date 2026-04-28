4cf1e008f90c9f0a9ceff80aa75b63bdd6d9144e
Martin Nilsson <ibbles@hotmail.com>
2026-04-28 21:46:36 +0200

    In mn.codex.fish, add --full_dir flag and use symlink to Unreal Engine installation

--- mn.codex.fish
+++ mn.codex.fish
@@ -1,21 +1,36 @@
 #!/usr/bin/env fish
 
-argparse 'h/help' 'i/inner_dir=' -- $argv
-or return
 
-echo "inner_dir arg: '$_flag_inner_dir'"
+argparse 'h/help' 'f/full_dir' 'i/inner_dir=' -- $argv
+or return
 
 set dirname (basename (pwd))
 set inner_dir "/cwd"
 
+if set -q _flag_help
+    echo "Run a Docker container with Codex installed and mount the current working directory."
+    echo ""
+    echo "-f --full_dir: Use the full current working directory path also in the Docker image."
+    echo "-i PATH --inner_dir=PATH: Directory inside the Docker container where the current working directory should be mounted. Overrides --full_dir."
+    exit 1
+end
+if set -q _flag_full_dir
+    set inner_dir (pwd)
+end
 if set -q _flag_inner_dir
     echo "Got inner_dir: '$_flag_inner_dir'."
     set inner_dir $_flag_inner_dir
 end
 
-set docker_args run -i -t --rm=true --security-opt seccomp=unconfined --name "Codex.$dirname" --user (id -u):(id -g) -v ~/.codex:/codex-home/ -v (realpath .):/"$inner_dir" -v /media/s2000/UnrealEngine/5.7.2:/UnrealEngine:ro -e CODEX_HOME=/codex-home $argv codex
+set docker_args run -i -t --rm=true \
+    --security-opt seccomp=unconfined \
+    --name "Codex.$dirname" \
+    --user (id -u):(id -g) \
+    -v $HOME/.codex:/codex-home/ -e CODEX_HOME=/codex-home \
+    -v (realpath .):/"$inner_dir" --workdir /"$inner_dir" \
+    -v $HOME/unreal_engine/:/UnrealEngine:ro \
+    codex
 
 echo docker (string escape -- $docker_args)
 
 docker $docker_args
-
