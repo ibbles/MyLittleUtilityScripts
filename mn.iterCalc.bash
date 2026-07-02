8757c5d36e592d3f5c342d5d8e63141624523a2d
Martin Nilsson <ibbles@hotmail.com>
2026-07-02 10:04:33 +0200

    In mn.iterCalc.bash, check for inotifywait

--- mn.iterCalc.bash
+++ mn.iterCalc.bash
@@ -4,6 +4,11 @@ if [ -z "${EDITOR}" ] ; then
     EDITOR=emacs
 fi
 
+if ! command -v inotifywait >/dev/null ; then
+    echo "This script needs 'inotifywait', on Ubuntu install the 'inotify-tools'."
+    exit 1
+fi
+
 touch iterCalc.m
 emacs iterCalc.m & disown
 
