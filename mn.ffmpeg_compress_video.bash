6fd56fc8c8a2ce5cd6222f5c1fb5f3e32c18c060
Martin Nilsson <ibbles@hotmail.com>
2026-07-14 21:50:36 +0200

    In mn.ffmpeg_compress_video.bash, use x265 instead of x264 and only include video0+audio0 streams
    Some videos contain data streams which ffmpeg doesn't know what to do
    with. Ignore those.

--- mn.ffmpeg_compress_video.bash
+++ mn.ffmpeg_compress_video.bash
@@ -42,19 +42,30 @@ name=`basename "$in_filename" ".$in_extension"`
 
 out_path=${dir_path}/${name}_compressed.mp4
 
+# Pick a compression level.
+# 34: not so good
+# 28: decent
+# 26: good
+# 24: great
+crf=28
+
 echo -e "Compressing\n'$in_path'\nto\n'${out_path}'."
 set -x
-ffmpeg \
-    -i "$in_path" \
-    -c:v libx264 \
-    -preset veryslow \
-    -qp 30 \
-    ${pix_fmt} \
-    "$out_path"
+time ffmpeg \
+    -i "$in_path" `# The file to compress.` \
+    -map 0:v:0 `# Use the first video stream.` \
+    -map 0:a:0 `# Use the first audio stream. ` \
+    `# A side-effect of the two -map command is that custom data streams are ignored.` \
+    -c:v libx265 `# Video compression library.` \
+    -preset slow `# Favor quality over compression speed.` \
+    -crf $crf `# Video compression level.` \
+    -tag:v hvc1 `# macOS baby-sitting.` \
+    -c:a aac `# Audio compression library.` \
+    -b:a 128k `# Audio bitrate.` \
+    -movflags +faststart `# Place frame metadata at the front so that the video can be streamed.` \
+    ${pix_fmt} `# Optionally force yuv420 output.` \
+    "$out_path" `# The file to create.`
 set +x
-# The '-qp #' parameter above can be tweaked to control quality / size.
-# A low number, such as 18, gives a large file size and high quality.
-# A high number, such as 30, gives a smaller file size and lower quality.
 
 if [ "$pix_fmt" = "" ] ; then
     echo -e "\n\nNote: Compressed with default pixel format, not 420. Make sure the compressed video can be played on the intended player before deleting the source file."
