9bd726738fce2b709b3edf2e44df00d73607392a
Martin Nilsson <ibbles@hotmail.com>
2026-07-25 16:41:24 +0200

    In mn.zellij_new_generic.fish, use 'zellij list-sessions' instead of 'ps aux'
    Didn't make the code smaller, but I still think this is a better
    solution.

--- mn.zellij_new_generic.fish
+++ mn.zellij_new_generic.fish
@@ -4,17 +4,29 @@
 # This script creates Zellij sessions named 'Generic #' where '#' is a number.
 
 
-# TODO Use 'zellij --list-sessions' instead of 'ps aux' in a loop.
-set id_end 51
-for id in (seq $id_end)
-    if ! ps aux | grep "zellij --session Generic $id" | grep -v "grep" > /dev/null
-        break
+set id 1
+
+# Loop until we either find a gap in the used IDs list, in case we use that ID,
+# or we run out of used IDs, in case we use the next ID.
+for used_id in (zellij list-sessions -n | grep  -oP "^Generic [0-9]+" | sort -n --key=2)
+    # Strip the "Generic " prefix.
+    set used_id (echo "$used_id" | cut -d ' ' -f2)
+
+    # If the used ID is smaller than the current ID, we have gotten out of sync
+    # in the iteration. That would be bad. Abort if so.
+    if test "$used_id" -lt "$id"
+        echo "Logic error: '$used_id < $id'."
+        exit 1
     end
-end
-if test "$id" = "$id_end"
-    echo "No free IDs available."
-    exit 1
+
+    # If the two IDs are different, then we have found a gap in the used IDs list.
+    # Use that ID for the new session.
+    if test "$used_id" != "$id"
+        break;
+    end
+
+    # This ID was already used, try the next one.
+    set id (expr $id + 1)
 end
 
 zellij --session "Generic $id"
-
