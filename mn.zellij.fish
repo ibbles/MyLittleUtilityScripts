4b2012a904d894722a7e7deaeb1ac5912a62744d
Martin Nilsson <ibbles@hotmail.com>
2026-08-03 07:07:15 +0200

    In mn.zellij.fish, add support for raw shell instead of Zellij container

--- mn.zellij.fish
+++ mn.zellij.fish
@@ -17,19 +17,30 @@ set menu_items
 for name in $names
     set menu_items $menu_items $name $name
 end
-set menu_items $menu_items "New Generic" "New Generic"
+set menu_items $menu_items \
+    "New Generic" "New Generic" \
+    "Bash Shell" "Bash Shell" \
+    "Fish Shell" "Fish Shell"
 
 # Display the menu to the user.
 set selected_name (dialog --menu "Select a session" 50 100 10 $menu_items 2>&1  >/dev/tty)
 echo ""
 echo "Selected session: $selected_name"
 
+# TODO The number of cases is getting longer, consider using a 'switch' instead
+# of else if chain.
+#
+# TODO Instead of hard-coding shell names, consider parsing '/etc/shells'.
 if test -z "$selected_name"
     # The user hit Esc or selected Cancel.
     echo "Nothing selected, doing nothing."
     exit 1
 else if test "$selected_name" = "New Generic"
     mn.zellij_new_generic.fish
+else if test "$selected_name" = "Bash Shell"
+    bash
+else if test "$selected_name" = "Fish Shell"
+    fish
 else
     zellij attach $selected_name
 end
