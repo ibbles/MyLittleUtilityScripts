ac6bb9d987ad4fd00c2eff1e4d7ebeca2403775b
Martin Nilsson <ibbles@hotmail.com>
2026-08-09 15:59:54 +0200

    In mn.ffmpeg_compress_video.bash, be explicit about color spaces and formats

--- mn.ffmpeg_compress_video.bash
+++ mn.ffmpeg_compress_video.bash
@@ -49,6 +49,13 @@ out_path=${dir_path}/${name}_compressed.mp4
 # 24: great
 crf=28
 
+# There are a bunch of color space options below. They have been chosen to
+# match videos recorded by a Pixel 9 smartphone. Find a way to collect options
+# for different use-cases and source material in an easily selectable way and
+# add a command line parameter for selecting one. Such as:
+#    mn.ffmpeg_compress_video.bash pixel5
+#    mn.ffmpeg_compress_video.bash screen_recording
+
 echo -e "Compressing\n'$in_path'\nto\n'${out_path}'."
 set -x
 time ffmpeg \
@@ -56,14 +63,27 @@ time ffmpeg \
     -map 0:v:0 `# Use the first video stream.` \
     -map 0:a:0 `# Use the first audio stream. ` \
     `# A side-effect of the two -map command is that custom data streams are ignored.` \
+    `#` \
+    `# Use full-range (pc) for both input and output.` \
+    -vf "scale=in_range=pc:out_range=pc,format=yuv420p" \
     -c:v libx265 `# Video compression library.` \
     -preset slow `# Favor quality over compression speed.` \
     -crf $crf `# Video compression level.` \
+    ${pix_fmt} `# Optionally force yuv420 output.` \
+    `# ` \
+    `# Color space metadata to write to the output.` \
+    -color_range pc `# Set full-range color.` \
+    -colorspace bt709 \
+    -color_primaries bt709 \
+    -color_trc bt709 \
+    `# ` \
+    `# Tell the encoder to use the same color space metadata.` \
+    `# The below must match the -color* flags aboge.` \
+    -x265-params "range=full:colorprim=bt709:transfer=bt709:colormatrix=bt709" \
     -tag:v hvc1 `# macOS baby-sitting.` \
     -c:a aac `# Audio compression library.` \
     -b:a 128k `# Audio bitrate.` \
     -movflags +faststart `# Place frame metadata at the front so that the video can be streamed.` \
-    ${pix_fmt} `# Optionally force yuv420 output.` \
     "$out_path" `# The file to create.`
 set +x
 
