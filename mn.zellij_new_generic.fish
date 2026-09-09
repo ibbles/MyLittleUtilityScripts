f7cdbe15a963a2efa06d7644e0457b5cd51755d3
Martin Nilsson <ibbles@hotmail.com>
2026-09-09 22:05:00 +0200

    In mn.zellij_new_generic.fish, reduce dependencies

--- mn.zellij_new_generic.fish
+++ mn.zellij_new_generic.fish
@@ -8,8 +8,8 @@ set id 1
 
 # Loop until we either find a gap in the used IDs list, in case we use that ID,
 # or we run out of used IDs, in case we use the next ID.
-for used_id in (zellij list-sessions -n | grep  -oP "^Generic [0-9]+" | sort -n --key=2)
-    # Strip the "Generic " prefix.
+for used_id in (zellij list-sessions -n | grep -oE '^Generic [0-9]+ ' | sort -n --key=2)
+    # Strip the "Generic " prefix and the session metadata.
     set used_id (echo "$used_id" | cut -d ' ' -f2)
 
     # If the used ID is smaller than the current ID, we have gotten out of sync
@@ -21,12 +21,12 @@ for used_id in (zellij list-sessions -n | grep  -oP "^Generic [0-9]+" | sort -n 
 
     # If the two IDs are different, then we have found a gap in the used IDs list.
     # Use that ID for the new session.
-    if test "$used_id" != "$id"
-        break;
+    if test "$used_id" -ne "$id"
+        break
     end
 
     # This ID was already used, try the next one.
-    set id (expr $id + 1)
+    set id (math $id + 1)
 end
 
 zellij --session "Generic $id"
